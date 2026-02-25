b323ad73e585aca625f10ee47bc225109d0a8b38
johnsonwane <64061435+johnsonwane@users.noreply.github.com>
2026-02-25 23:03:12 +0800

    将默认数据库名调整为 oa2

--- api/config.php
+++ api/config.php
@@ -4,7 +4,7 @@ return [
     'db' => [
         'host' => getenv('DB_HOST') ?: '10.0.0.7',
         'port' => getenv('DB_PORT') ?: '3306',
-        'dbname' => getenv('DB_NAME') ?: 'oa_codex',
+        'dbname' => getenv('DB_NAME') ?: 'oa2',
         'user' => getenv('DB_USER') ?: 'root',
         'pass' => getenv('DB_PASS') ?: '?(kFMNWb)5Fv+8-_uYwG',
         'charset' => 'utf8mb4',
